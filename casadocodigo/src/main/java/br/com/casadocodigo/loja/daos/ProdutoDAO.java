db09bf201a40b71761cc5429a0315d9df0bfb52e
Vinicius Pasquantonio <vinicius.pasquantonio@gmail.com>
2016-12-23 00:07:29 -0200

    configuracoes jpa

---
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/daos/ProdutoDAO.java
@@ -0,0 +1,21 @@
+package br.com.casadocodigo.loja.daos;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.casadocodigo.loja.models.Produto;
+
+@Repository
+@Transactional
+public class ProdutoDAO {
+
+	@PersistenceContext
+    private EntityManager manager;
+
+    public void gravar(Produto produto){
+        manager.persist(produto);
+    }
+}
